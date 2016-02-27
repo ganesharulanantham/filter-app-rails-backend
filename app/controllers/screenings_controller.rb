bcd367caf270ad622a1313a73073ab98a769dfff
Ganesh Arulanantham <ganesh.hobbeez@gmail.com>
2016-02-27 17:40:29 +0530

    Basic filter functionality

--- app/controllers/screenings_controller.rb
+++ app/controllers/screenings_controller.rb
@@ -1,7 +1,24 @@
 class ScreeningsController < ApplicationController
-  
+
+  skip_before_action :verify_authenticity_token
+
   def index
     render json: Screening.all.to_json
   end
 
+  def filters
+    render json: Screening.where(construct_query_string).to_json
+  end
+
+  private
+  
+  def construct_query_string
+    str=''
+    params[:attributes].each_with_index do |e, index|
+    str += " and " if index != 0
+    str += e[0].to_s
+    str += " = " +"'" +e[1][:value] + "'" if e[1][:operator] == "is_equal_to"
+    end
+    str
+  end
 end
